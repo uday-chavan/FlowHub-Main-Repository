f9a816cc47abeceaf34135e75720115216e7e216
Uday <chavanuday407@gmail.com>
2025-09-20 10:06:31 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist/client'
+    outDir: '../dist/client'
   },
   root: './client',
   resolve: {
