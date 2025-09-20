7a4865cd5e18291558fcd6db26b6c0f2a3b67228
Uday <chavanuday407@gmail.com>
2025-09-20 10:00:04 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
@@ -7,6 +8,11 @@ export default defineConfig({
     outDir: 'dist/client'
   },
   root: './client',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './client/src')
+    }
+  },
   server: {
     port: 5173
   }
