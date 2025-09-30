82af54cc40dec141a335c7bf3d78a1e6d545fd54
Uday <chavanuday407@gmail.com>
2025-10-01 01:05:13 +0530

    Update use-mobile.tsx

--- client/src/hooks/use-mobile.tsx
+++ client/src/hooks/use-mobile.tsx
@@ -1,36 +1,20 @@
+
 import * as React from "react"
 
-const MOBILE_BREAKPOINT = 1545
+const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
 
   React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT}px)`)
+    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
     const onChange = () => {
-      setIsMobile(window.innerWidth <= MOBILE_BREAKPOINT)
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
     }
     mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth <= MOBILE_BREAKPOINT)
+    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
     return () => mql.removeEventListener("change", onChange)
   }, [])
 
   return !!isMobile
-}
-
-// Additional hook for intermediate breakpoints
-export function useIsCompactView() {
-  const [isCompact, setIsCompact] = React.useState<boolean | undefined>(undefined)
-
-  React.useEffect(() => {
-    const mql = window.matchMedia("(max-width: 1133px)")
-    const onChange = () => {
-      setIsCompact(window.innerWidth <= 1133)
-    }
-    mql.addEventListener("change", onChange)
-    setIsCompact(window.innerWidth <= 1133)
-    return () => mql.removeEventListener("change", onChange)
-  }, [])
-
-  return !!isCompact
 }
